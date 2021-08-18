951f4cb5d171192133b2d3e8e1754300ac85afd4
Abhishek <abhibhovar@gmail.com>
2021-08-18 09:50:28 +0530

    Modified

--- day#2.sql
+++ day#2.sql
@@ -1,4 +1,24 @@
 use DemoProj;
 
 -- Constraints:  Rules to get efficient working of the data base 
--- Accuracy, Consistency and Reliability 
+-- Accuracy, Consistency, Reliability and Validity of data is taken care by the Constraint
+-- Types: {Primary Key, Unique, Foreign Key, Not Null, Check, Default}
+
+insert into Customers(cust_id, cust_name) values (200,  null);
+
+-- In RDBMS no two rows should be same 
+select * from Customers;
+
+alter table Customers alter column cust_id int not null; -- not null constraint for cust_id
+-- as we dont want cust_id to be null
+
+-- this wont allow the following entry as the id we have passed is null
+-- constraint for the cust_id is not null
+insert into Customers(cust_id, cust_name) values (null,  'Abhishek');
+
+delete from Customers where cust_id is null;-- this will delete all the records with null entries
+delete from Customers where cust_name is null;-- this will delete all the records with null entries
+
+alter table Customers alter column cust_name varchar(30) not null;
+
+
