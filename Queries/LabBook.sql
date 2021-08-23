a895f0bc732877f8eac7d4f41fff00ec7a1fdf74
Abhishek <abhibhovar@gmail.com>
2021-08-23 09:33:32 +0530

    Modified

--- Queries/LabBook.sql
+++ Queries/LabBook.sql
@@ -13,23 +13,45 @@ create table Customer(
 
 ---------<Creating User Defined D_Type>---------
 create type Region from varchar(15) ; -- create what ? -> Type, What should be its name -> Region, Using What -> varchar, Length -> 15
-
-
-create procedure define
-as 
-begin 
-	declare @add Region ;
-	set @add = 'Andheri East Pump House'
-end 
-
 -- <Procedure{Several Queries in Single command}>
 
 alter procedure define -- defining Procedure to declare and use the Region type data
 as 
 begin 
 	declare @add Region ; -- declaraing the Region type data named add
-	set @add = 'Andheri East' -- giving value to add
 	print @add -- displaying add
 end 
 
-define -- executing the procedure 
+drop procedure define ;
+-- <Creating a Default> --
+create default DefRegion as 'NA';
+
+sp_bindefault DefRegion, Region ;
+
+exec define ;
+
+
+alter table Customer add custregion Region ;
+alter table Customer add Gender char(1) ;
+
+sp_help Customer;
+
+alter table Customer 
+add constraint ck_gnder check(Gender = 'M' or Gender = 'F' or Gender = 'T' );
+
+alter table Customer 
+drop constraint ck_gnder;
+
+alter table Customer 
+add constraint ck_gender check(Gender = 'M' or Gender = 'F' or Gender = 'T');
+
+
+create table Orders(
+	oid int not null check(oid>10000),
+	cid int not null,
+	odate Datetime,
+	ostate char(1) check(ostate='P' or ostate = 'C')
+);
+
+
+
---
+++ Queries/day#5.sql
@@ -0,0 +1,10 @@
+--> Business Rule : Real life use {Railway, Account, Hospital} management 
+
+-------------- <Triggers> --------------
+--> works with ddl{create alter drop} and dml{insert update delete merge} commands 
+--> two types after and instate of trigger 
+--> For Complex validation
+
+--> here we only have after trigger 
+--> after a statement{insert, delete, update} is executed trigger would be fired 
+
