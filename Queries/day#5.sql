6af5bd1ae4fab7e8cda169302463ec84ce337ba4
Abhishek <abhibhovar@gmail.com>
2021-08-23 10:00:08 +0530

    Modified

--- Queries/day#5.sql
+++ Queries/day#5.sql
@@ -2,9 +2,63 @@
 
 -------------- <Triggers> --------------
 --> works with ddl{create alter drop} and dml{insert update delete merge} commands 
---> two types after and instate of trigger 
+--> two types after and instead of trigger 
 --> For Complex validation
 
 --> here we only have after trigger 
---> after a statement{insert, delete, update} is executed trigger would be fired 
+--> after a statement{insert, delete, update} is executed trigger would be fired
+select name from master.sys.databases;
+
+create database CustomerDb;
+use CustomerDb;
+
+create table tab1(
+	Rno int,
+	s1 int,
+);
+
+
+-- <After Trigger> --
+--{ --> we want to get the error message --> and den roll back transaction } --> can be done by constraint
+--> we dont have to do it explicitly
+
+
+create trigger i_trigger  --> creating a trigger 
+on tab1 --> for table tab1
+for insert --> for action insert 
+as 
+begin 
+	declare @sub int
+	select @sub = s.s1 from inserted s;
+	if(@sub > 100) --> checking conditon
+		begin
+			print('Invalid Marks Entry')
+			rollback tran --> rolling back the transaction if invalid entry 
+		end 
+	else 
+		print('Inserted Successfully ')
+end 
+
+
+insert into tab1 values(1, 23);
+insert into tab1 values(2, 230);
+insert into tab1 values(2, 100);
+
+
+-- <Instead of Trigger> --
+--> Meaning : instead of trigger do something else 
+-- delete from tab1 where Rno = 1; --> this is will delete data where id is 1 from table tab1
+--> if we dont want this to happen we can create an instead 
+
+create trigger no_del on tab1 --> creating trigger on tab1
+instead of delete --> for instead of delete
+as 
+begin --> what to do instead of delete 
+	print('Deleting record is not permissible') --> print this message 
+end 
+
+delete from tab1 where Rno = 1;
+
+select * from tab1;
+
 
